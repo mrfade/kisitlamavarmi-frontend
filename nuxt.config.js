247869bd08ea316c5287db5bf60eedbe82abbf6f
Enes <enessolak99@gmail.com>
2021-03-04 01:57:48 +0200

    meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'kisitlamavarmi',
+    title: 'Kısıtlama Var Mı?',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Bugün kısıtlama var mı? Şehrimde kısıtlamalar var mı? Sokağa çıkabilir miyim?',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
